6a3ef34bbb67060092e7b10d1c44d54dc321a31a
yatushaa <y.vigneswaran98@gmail.com>
2021-03-31 17:35:34 +0200

    comming my first script

---
+++ code_v0.2.R
@@ -0,0 +1,3 @@
+install.packages("dslabs")  # to install a single package
+install.packages(c("tidyverse", "dslabs")） # to install two packages at the same time
+                 installed.packages() # to see the list of all installed packages
